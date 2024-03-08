d081955dae07cf0ec780ee96c3e26879d4b0cdc3
Natanssilva <natanssilva10@gmail.com>
2024-03-08 00:27:56 -0300

    first commit

---
+++ src/login.tsx
@@ -0,0 +1,16 @@
+
+import './App.css';
+import { Card } from './components/ui/card';
+
+
+function App() {
+  return (
+    <div className="bg-sky-500">
+      <Card>
+        Card
+      </Card>
+    </div>
+  );
+}
+
+export default App;
